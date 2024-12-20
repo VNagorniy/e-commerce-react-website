f7ab7ef7b6de42c29b2ed2b811fda5da97d5ed53
VNagorniy <vnagorniyrnd@gmail.com>
2024-12-20 16:31:38 +0300

    init: start a project with a workpiece

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Announcement from '../../components/announcement/Announcement';
+
+function Home() {
+	return (
+		<div>
+			<Announcement />
+		</div>
+	);
+}
+
+export default Home;
