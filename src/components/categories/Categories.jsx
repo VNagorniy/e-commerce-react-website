8a814ad513eef3a0f21907f14741e63a5951b60e
VNagorniy <vnagorniyrnd@gmail.com>
2024-12-20 21:04:07 +0300

    feat: add categories block

---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,19 @@
+import { categories } from '../../data';
+import './Categories.css';
+function Categories() {
+	return (
+		<div className="categories-container">
+			{categories.map((item) => (
+				<div key={item.id} className="category-container">
+					<img src={item.img} alt="" />
+					<div className="category-info">
+						<h1>{item.title}</h1>
+						<button>Shop Now</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default Categories;
