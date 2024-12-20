ae073c2594c4f8e26c34515c9c5a7b9e3ec2ad33
VNagorniy <vnagorniyrnd@gmail.com>
2024-12-20 20:58:05 +0300

    feat: add slider block

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Announcement from '../../components/announcement/Announcement';
 import Navbar from '../../components/navbar/Navbar';
+import Slider from '../../components/slider/Slider';
 
 function Home() {
 	return (
 		<div>
 			<Announcement />
 			<Navbar />
+			<Slider />
 		</div>
 	);
 }
