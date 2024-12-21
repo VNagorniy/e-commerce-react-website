bf6b81cbfdb20c2880a2459a68749c8b8b3aeea0
VNagorniy <vnagorniyrnd@gmail.com>
2024-12-21 17:16:46 +0300

    feat: add login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</Router>
 	);
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -25,7 +25,9 @@ function Navbar() {
 					<Link to="/register">
 						<div className="navbar-menuItem">Register</div>
 					</Link>
-					<div className="navbar-menuItem">Login</div>
+					<Link to="/login">
+						<div className="navbar-menuItem">Login</div>
+					</Link>
 					<div className="navbar-menuItem">
 						<MdOutlineShoppingCart className="icon" />
 						<span className="cart-badge"></span>
