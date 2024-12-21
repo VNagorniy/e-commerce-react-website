f805b3ff51560880da957c2e30e06e897801f83a
VNagorniy <vnagorniyrnd@gmail.com>
2024-12-21 16:19:23 +0300

    feat: add newsletter block

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from '../../components/navbar/Navbar';
 import Bestsellers from '../../components/products/Bestsellers';
 import NewArrivals from '../../components/products/NewArrivals';
 import Slider from '../../components/slider/Slider';
+import Newsletter from '../../components/newsletter/Newsletter';
 
 function Home() {
 	return (
@@ -14,6 +15,7 @@ function Home() {
 			<Categories />
 			<NewArrivals />
 			<Bestsellers />
+			<Newsletter />
 		</div>
 	);
 }
